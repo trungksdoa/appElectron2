2798f38122d6e722e3057d6d70f6012b56a77a8b
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-04-10 22:23:44 +0700

    ABC

--- src/main/main.ts
+++ src/main/main.ts
@@ -211,8 +211,8 @@ autoUpdater.on('checking-for-update', () => {
 autoUpdater.on('update-available', () => {
   sendStatusToWindow('Update available.');
 });
-autoUpdater.on('update-not-available', (event, releaseNotes, releaseName) => {
-  sendStatusToWindow(`${event} - ${releaseNotes} - ${releaseName}`);
+autoUpdater.on('update-not-available', (event) => {
+  sendStatusToWindow(`${event}`);
 });
 autoUpdater.on('error', (err) => {
   sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
