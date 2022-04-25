79f44875e05aa027a3ebafd030a4f791f44b66c3
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-04-25 11:52:49 +0700

    sda

--- src/main/main.ts
+++ src/main/main.ts
@@ -120,7 +120,6 @@ const createWindow = async () => {
 
   ipcMain.on('ipc-example', async (event, arg) => {
     const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-    console.log(msgTemplate(arg));
     event.reply('ipc-example', msgTemplate('pong'));
   });
 
